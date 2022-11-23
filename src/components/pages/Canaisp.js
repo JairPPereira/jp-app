abf01efdb466b8254f26de8d3c5b0db1164dd2d6
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-11-23 11:13:52 -0300

    yy

--- src/components/pages/Canaisp.js
+++ src/components/pages/Canaisp.js
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,37 +16,159 @@ export default function Canaisp() {
   return (
     <><center><h1>Funciona no momento apenas em dispositivos mobile</h1></center>
 
-        <center>
-          <p></p>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://images.pluto.tv/channels/5f121262a189a800076b9386/colorLogoSVG.svg?auto=&fit=crop&fill=blur&ixlib=react-9.1.5&w=150&h=90&dpr=1&q=75" />
-      <Card.Body>
-        <Card.Title>Canal Séries</Card.Title>
-        <Card.Text><p2></p2>
-        </Card.Text>
-        <Link to="./Plutoser"><Button variant="primary" size="lg" width="100%">
-      Assistir
-        </Button></Link>
-      </Card.Body>
-    </Card></center>
+    
+<Row>
 
-    <center>
-          <p></p>
-      <Card style={{ width: '18rem' }}>
+
+    <Col> 
+    
+    <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://images.pluto.tv/channels/5f997e44949bc70007a6941e/colorLogoSVG.svg?auto=&fit=crop&fill=blur&ixlib=react-9.1.5&w=150&h=90&dpr=1&q=75" />
       <Card.Body>
-        <Card.Title>Canal Turma da Monica</Card.Title>
-        <Card.Text><p2></p2>
-        </Card.Text>
+
         <Link to="./Plutotdam"><Button variant="primary" size="lg" width="100%">
       Assistir
         </Button></Link>
       </Card.Body>
-    </Card></center>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/5f12101f0b12f00007844c7c/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
 
+        <Link to="/plutocom"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/5f120f41b7d403000783a6d6/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
 
+        <Link to="/plutoacao"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    </Col>
+
+    <Col> 
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/5f121262a189a800076b9386/colorLogoSVG.svg?auto=&fit=crop&fill=blur&ixlib=react-9.1.5&w=150&h=90&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="./Plutoser"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/61b790b985706b00072cb797/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="/plutoaf"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    </Col>
+
+    <Col> 
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/5f120e94a5714d00074576a1/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="/plutosucesso"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/62545ed3dab4380007582f7c/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="/plutocomr"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/62545ed3dab4380007582f7c/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="/record"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    
+    </Col>
+
+    <Col> 
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/5f171f988ab9780007fa95ea/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="/plutoromance"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/5fa15ad6367e170007cdd098/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="/plutofc"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    </Col>
+
+    <Col> 
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/5f5a545d0dbf7f0007c09408/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="/plutonac"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pluto.tv/channels/5f1210d14ae1f80007bafb1d/featuredImage.jpg?auto=&ixlib=react-9.1.5&w=224&h=124&dpr=1&q=75" />
+      <Card.Body>
+
+        <Link to="/plutodrama"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card>
 
     
+    </Col>
+
+ 
+    
+      
+   
+
+   
+
+   
+
+   
+
+   
+
+   
+
+   
+
+
+
+
+    </Row>
 
           <p><br /></p>
 
